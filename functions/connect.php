9e5ac96f526664cef705a5c8972214ee3b1ec80c
nowaxik <nowaxik@gmail.com>
2018-11-05 21:37:29 +0100

    0.2.1
    Add file connect to database

---
+++ functions/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $conn = new mysqli($servername, $username, $password);
+
+    //Spradzenie połączenia
+    if ($conn->connect_error) {
+        die("Błąd połącznenia: " . $conn->connect_error);
+    } 
+        echo "Połączono z bazą danych";
+?>
