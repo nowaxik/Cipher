11bf6f462a3ba3c04249c7094f370ade7ef66ca5
nowaxik <nowaxik@gmail.com>
2018-11-05 21:40:29 +0100

    0.2.2
    Add comment

--- functions/connect.php
+++ functions/connect.php
@@ -1,11 +1,12 @@
 <?php
-
+    
+    //Połącznie z bazą danych
     $servername = "localhost";
     $username = "root";
     $password = "";
 
     $conn = new mysqli($servername, $username, $password);
-
+    
     //Spradzenie połączenia
     if ($conn->connect_error) {
         die("Błąd połącznenia: " . $conn->connect_error);
