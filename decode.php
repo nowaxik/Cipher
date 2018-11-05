2574e39b5fe0dcca3952f9887da28f04d8bf9a42
nowaxik <nowaxik@gmail.com>
2018-11-05 23:20:34 +0100

    0.2.7
    Add HTML tags and CSS

--- decode.php
+++ decode.php
@@ -1,20 +1,59 @@
-<?php
-
-/**
- * @author Marcin
- * @copyright 2015
- */
- 
- 	//Pobranie tekstu z formularza
-    $posts = $_POST['recode'];
- 	
-	//Wczytanie zawartości pliku
-    include 'rekey.php';
-	
-	//Zmiana tekstu na ciąg znaków
-    $p = strtr($posts, $arry + $arrypluss + $arryplus);
-	
-	//Wyświetlenie
-    echo $p
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="Silence / REPT" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     
-?>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   
+    <!-- Moje własne style CSS -->
+    <link rel="Stylesheet" type="text/css" href="css/style.css" />
+    
+	<title>Cipher</title>
+
+</head>
+
+<body>
+    
+    <!-- Główne okno programu -->
+    
+    <div class="container">
+         <div class="row">
+            <div class="col-md-6">
+                <h3>Oto rozszyfrowany tekst</h3>
+                    <br/>
+                        <?php
+
+                            //Pobranie tekstu z formularza
+                            $posts = $_POST['recode'];
+
+                            //Wczytanie zawartości pliku
+                            include 'rekey.php';
+
+                            //Zmiana tekstu na ciąg znaków
+                            $p = strtr($posts, $arry + $arrypluss + $arryplus);
+
+                            //Wyświetlenie
+                            echo $p
+
+                        ?>
+                    <br/><br/>
+                <a class="btn btn-primary" href="index.php" role="button">Wstecz</a>
+                
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+</body>
+</html>
